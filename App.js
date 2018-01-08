200f6224b4e83a889cdd34e06a55a728bbea94d5
Daniel Kim <daniel.jaewoo.kim@gmail.com>
2018-01-08 04:40:55 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>IQP 3 Project Runaways!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Button onPress={function() {}} title="Test Button" color="#841584" accessibilityLabel="YO SUP" />
       </View>
     );
   }
