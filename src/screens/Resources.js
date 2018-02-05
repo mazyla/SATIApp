84f770cc1452d90d5835b8a81ecd02613815a1bf
Mario Zyla <mazyla@wpi.edu>
2018-02-05 13:50:51 +0700

    Showing some resources and opening links on Google maps

--- src/screens/Resources.js
+++ src/screens/Resources.js
@@ -1,7 +1,9 @@
+import TouchableOpacity from 'TouchableOpacity';
 import React, {Component} from 'react';
-import { View, Image, Dimensions} from 'react-native';
+import { View, Image, Dimensions, Text} from 'react-native';
 import styles from '../styles/styles.js';
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import openMap from 'react-native-open-maps';
 
 
 const width = Dimensions.get('window').width
@@ -17,7 +19,6 @@ const LATITUDE_DELTA = 0.01;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 
-
 export default class ResourcesView extends React.Component {
   constructor(props) {
     super(props);
@@ -31,16 +32,57 @@ export default class ResourcesView extends React.Component {
         latitudeDelta: LATITUDE_DELTA,
         longitudeDelta: LONGITUDE_DELTA
       },
-      marker: {
+      markers: [
+        {
         coordinate: {
           latitude: 13.740331,
           longitude: 100.514245,
         },
         title: "The Hub",
-      }
+      },
+        {
+          coordinate: {
+            latitude: 13.759573,
+            longitude: 100.497486,
+          },
+          title: "Surat Clinic",
+        },
+        {
+          coordinate: {
+            latitude: 13.760883,
+            longitude: 100.555128,
+          },
+          title: "คลินิก สวท เวชกรรม ดินแดง",
+        }
+      ],
 
     };
   }
+
+  _goTo(title) {
+    var lat;
+    var long;
+    switch(title) {
+      case 'The Hub':
+        lat = 13.740331;
+        long = 100.514245;
+        break;
+      case 'Surat Clinic':
+        lat = 13.759573;
+        long = 100.497486;
+        break;
+      case 'คลินิก สวท เวชกรรม ดินแดง':
+        lat = 13.760883;
+        long = 100.555128;
+        break;
+      default:
+        lat = LATITUDE;
+        long = LONGITUDE;
+        break;
+    }
+      openMap({ latitude: lat, longitude: long, provider: PROVIDER_GOOGLE, zoom: 18 });
+    }
+
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
@@ -63,6 +105,7 @@ export default class ResourcesView extends React.Component {
 
   render() {
     return (
+      <View>
       <MapView
         style={styles.map}
         provider={ PROVIDER_GOOGLE }
@@ -71,13 +114,28 @@ export default class ResourcesView extends React.Component {
         followUserLocation={true}
         showsCompass={true}
         showsBuildings={true} >
+        {this.state.markers.map(marker => (
+           <Marker
+             key={marker.key}
+             coordinate={marker.coordinate}
+             pinColor={marker.color}
+             // title={marker.title}
+             // title={marker.title}
+             // onPress={() => this._openResourceDialog(marker.title)}
+             onPress={this.showCallout}
+             //onCalloutPress={this._goTo(marker.title)}
+             //onPress={() => {this._goTo(marker.title)}}
+           >
+           <Callout tooltip={false}
+            onPress={() => {this._goTo(marker.title)}}>
+            <Text>{marker.title}</Text>
+            </Callout>
+           </Marker>
 
+         ))}
+       </MapView>
+       </View>
+);
 
-          <Marker
-            coordinate={this.state.marker.coordinate}
-            title={this.state.marker.title}
-          />
-        </MapView>
-    );
   }
 }
